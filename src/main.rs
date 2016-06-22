f1270a7b60ca77ff252055f99325627ab43de31d
Cyberunner23 <narutoxela@gmail.com>
2016-06-22 15:49:33 -0400

    Added initial Rust code.

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+//Copyright (c) 2016, Ruslan Baratov, Alex Frappier Lachapelle
+//All rights reserved.
+
+
+////////////////////////////////////////////////////////////
+//                          Main                          //
+////////////////////////////////////////////////////////////
+
+fn main() {
+
+    let hunter_bot_version = "1.0.0-dev";
+
+    //Print welcome message
+    println!("╔══════════════════════════════════════════════════════════╗");
+    println!("║                Welcome to the Hunter Bot.                ║");
+    println!("╚══════════════════════════════════════════════════════════╝");
+
+}
